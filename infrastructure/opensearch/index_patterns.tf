43299d9011b1e586d0562805590c7f587cac47bc
Đỗ Trọng Hải <41283691+hainenber@users.noreply.github.com>
2025-04-26 15:36:27 +0700

    feat(infra/opensearch-dashboard): generate OpenSearch dashboard objects (vis,saved search and index aptterns) with built-in Terraform `templatefile` (#11)
    * feat(infra/opensearch-dashboard): generate OpenSearch dashboard objects (vis,saved search and index aptterns) with built-in Terraform `templatefile`
    
    Signed-off-by: hainenber <dotronghai96@gmail.com>
    
    * doc(readme): update `Getting Started` section
    
    Signed-off-by: hainenber <dotronghai96@gmail.com>
    
    ---------
    
    Signed-off-by: hainenber <dotronghai96@gmail.com>

--- infrastructure/opensearch/index_patterns.tf
+++ infrastructure/opensearch/index_patterns.tf
@@ -1,17 +1,4 @@
 resource "opensearch_dashboard_object" "github-security-advisory-database" {
   tenant_name = local.default_tenant_name
-  body        = <<EOF
-  [
-    {
-      "_id": "index-pattern:github-security-advisory-database",
-      "_source": {
-        "type": "index-pattern",
-        "index-pattern": {
-          "title": "github-security-advisory-database",
-          "timeFieldName": "published"
-        }
-      }
-    }
-  ]
-EOF
+  body        = file("./index_patterns/github_security_advisory_database.json")
 }
--- infrastructure/opensearch/saved_searches.tf
+++ infrastructure/opensearch/saved_searches.tf
@@ -1,62 +1,13 @@
 resource "opensearch_dashboard_object" "raw-ghsa-vuln-data" {
   tenant_name = local.default_tenant_name
-  body        = <<EOF
-  [
-        {
-            "_id": "search:raw-ghsa-vuln-data",
-            "_source": {
-                "type": "search",
-                "search": {
-                    "columns": [
-                        "_source"
-                    ],
-                    "kibanaSavedObjectMeta": {
-                        "searchSourceJSON": "{\"query\":{\"query\":\"\",\"language\":\"kuery\"},\"highlightAll\":true,\"version\":true,\"aggs\":{\"2\":{\"date_histogram\":{\"field\":\"published\",\"calendar_interval\":\"1y\",\"time_zone\":\"Asia/Saigon\",\"min_doc_count\":1}}},\"filter\":[],\"indexRefName\":\"kibanaSavedObjectMeta.searchSourceJSON.index\"}"
-                    },
-                    "title": "Raw GHSA vulnerabilities"
-                },
-                "references": [
-                    {
-                        "id": "github-security-advisory-database",
-                        "name": "kibanaSavedObjectMeta.searchSourceJSON.index",
-                        "type": "index-pattern"
-                    }
-                ]
-            }
-        }
-    ]
-EOF
+  body = templatefile("./saved_searches/raw_ghsa_vuln_data.tftpl", {
+    searchSourceJSON = jsonencode(file("./saved_searches/raw_ghsa_vuln_data.searchSource.json"))
+  })
 }
 
 resource "opensearch_dashboard_object" "unfixed-ghsa-reviewd-vuln-data" {
   tenant_name = local.default_tenant_name
-  body        = <<EOF
-  [
-        {
-            "_id": "search:unfixed-ghsa-reviewed-vuln-data",
-            "_source": {
-                "type": "search",
-                "search": {
-                    "columns": [
-                        "database_specific.severity",
-                        "summary",
-                        "affected.package.ecosystem",
-                        "affected.package.name"
-                    ],
-                    "kibanaSavedObjectMeta": {
-                        "searchSourceJSON": "{\"query\":{\"query\":\"database_specific.github_reviewed: true AND NOT affected.ranges.events.fixed:*\",\"language\":\"kuery\"},\"highlightAll\":true,\"version\":true,\"aggs\":{\"2\":{\"date_histogram\":{\"field\":\"published\",\"calendar_interval\":\"1y\",\"time_zone\":\"Asia/Saigon\",\"min_doc_count\":1}}},\"filter\":[],\"indexRefName\":\"kibanaSavedObjectMeta.searchSourceJSON.index\"}"
-                    },
-                    "title": "Unfixed GHSA-reviewed vulnerabilities"
-                },
-                "references": [
-                    {
-                        "id": "github-security-advisory-database",
-                        "name": "kibanaSavedObjectMeta.searchSourceJSON.index",
-                        "type": "index-pattern"
-                    }
-                ]
-            }
-        }
-    ]
-EOF
+  body = templatefile("./saved_searches/unfixed_ghsa_reviewed_vuln_data.tftpl", {
+    searchSourceJSON = jsonencode(file("./saved_searches/unfixed_ghsa_reviewed_vuln_data.searchSource.json"))
+  })
 }
