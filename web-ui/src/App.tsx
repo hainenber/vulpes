9126eb47692dec87207302d0a9b652421803a4be
hainenber <dotronghai96@gmail.com>
2025-05-02 18:57:36 +0700

    feat(web-ui): migrate to React 19

--- web-ui/src/App.tsx
+++ web-ui/src/App.tsx
@@ -2,6 +2,7 @@ import { Refine } from "@refinedev/core";
 
 import { dataProvider } from "./providers/data-provider";
 import { ShowAuditLog } from "./pages/audit-logs/show";
+import { JSX } from "react";
 
 export default function App(): JSX.Element {
   return (
