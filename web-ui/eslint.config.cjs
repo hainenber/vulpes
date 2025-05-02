b16c4c0756b57782f7cfcd6622e28675552cb8bf
hainenber <dotronghai96@gmail.com>
2025-05-02 15:12:17 +0700

    chore(web-ui): migrate to ESLint 9

---
+++ web-ui/eslint.config.cjs
@@ -0,0 +1,36 @@
+const globals = require("globals");
+const { defineConfig, globalIgnores } = require("eslint/config");
+const typescriptParser = require("@typescript-eslint/parser");
+const reactRefreshPlugin = require("eslint-plugin-react-refresh");
+const recommendedTypescriptLinterPlugin = require("@typescript-eslint/eslint-plugin");
+const recommendedReactHooksPlugin = require('eslint-plugin-react-hooks')
+
+module.exports = defineConfig([
+    // Ignore dist/ folder and own ESLint config from being linted.
+    globalIgnores([
+        '**/dist/',
+    ]),
+
+    // Migrate from original refine.dev-scaffolded ESLint 8 config.
+    {
+      languageOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        globals: {
+          ...globals.browser
+        },
+        parser: typescriptParser,
+      },
+
+      plugins: {
+        "react-refresh": reactRefreshPlugin,
+        "react-hooks/recommended": recommendedReactHooksPlugin,
+        "@typescript-eslint/recommended": recommendedTypescriptLinterPlugin
+      },
+
+      // Override the recommended config.
+      rules: {
+        "react-refresh/only-export-components": "warn",
+      },
+    }
+]);
